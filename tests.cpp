cb36a400057b614b751cec5bed2b440fd4b33c5f
stepgenn <s.Gennertova@seznam.cz>
2018-03-01 10:25:45 +0100

    onet test for get_infile_length

--- tests.cpp
+++ tests.cpp
@@ -10,6 +10,18 @@
 
 TEST_CASE("Hello word","hello") {
 	CHECK(1==1);
-	CHECK(1==0);
+	CHECK(1!=0);
+}
+
+TEST_CASE("get_infile_length","function test") {
+	std::ofstream test_file;
+	test_file.open("test_file",std::ios::binary);
+	char string[]="This will be written in file.";
+	test_file.write(string,string.size());
+	test_file.close();
+
+	std::ifstream input_file;
+	input_file.open("test_file");
+	CHECK(string.size() == get_infile_length(input_file));
 }
 
